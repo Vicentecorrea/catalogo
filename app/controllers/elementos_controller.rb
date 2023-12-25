1bb59d3e45bd663d49c937559cffc078d1195278
vcorrea <vcorrea@buk.cl>
2023-12-24 21:06:52 -0300

    style: mantener mayúsculas y minúsculas en input

--- app/controllers/elementos_controller.rb
+++ app/controllers/elementos_controller.rb
@@ -19,22 +19,14 @@ class ElementosController < ApplicationController
       'biblioteca',
       'espacio',
     ]
-    @searched_text = params[:searched_text].downcase
+    @searched_text = params[:searched_text]
+    @searched_text_downcase = @searched_text.downcase
 
     query_text = ''
     @searchable_attributes.each_with_index do |attr, index|
-      query_text << "lower(#{attr}) like \'%#{@searched_text}%\' or "
+      query_text << "lower(#{attr}) like \'%#{@searched_text_downcase}%\' or "
     end
     query_text = query_text[..-4]
     @results = Elemento.where(query_text)
-
-    # Para mostrar resultados separados por atributo
-    # @results = {}
-    # @searchable_attributes.each do |attr|
-    #   elementos = Elemento.where("lower(#{attr}) like \'%#{@searched_text}%\'")
-    #   if elementos.any?
-    #     @results[attr] = elementos
-    #   end
-    # end
   end
 end
